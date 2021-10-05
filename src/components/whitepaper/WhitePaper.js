996bd2625ccd75aaa492cd4b6d2738fdee611df8
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2021-10-05 15:18:04 +0100

    whitespace

--- src/components/whitepaper/WhitePaper.js
+++ src/components/whitepaper/WhitePaper.js
@@ -11,13 +11,12 @@ export default class WhitePaper extends Component {
                 <h2>Ok, ok, ok...</h2>
                 <p>This is just for fun. The blockchain is written in Python so is in no way robust or scalable enough to take it all the way to the moon 🚀 ⛔️ 🌕.</p>
                 <p>What it does do is give us a playground to make transactions on a blockchain, building up a (hopefully) interesting set of data.</p>
-                <p>To make this more interesting the value of the currency will be manipulated in a couple of different ways, meaning there is likelihood for wins and losses. Don't worry though as the money is fake and you can always refresh your screen to gain a new wallet and balance.</p>
+                <p>To make this more interesting the value of the currency will be manipulated in a couple of different ways, meaning there is a likelihood for wins and losses. Don't worry though as the money is fake and you can always refresh your screen to gain a new wallet and balance.</p>
                 <p>Once we've done this a couple of times we will take a look at the blockchains.</p>
                 <p>I will also share the code with those who are interested in either this super stylish frontend or the boring old blockchain backend.</p>
                 <p>Let's go make some 💰💰💰</p>
                 <br></br>
                 <p><b>*</b>FUD is a term used by crypto enthusiasts to put down people concerned with particular investments: Fear, Uncertainty and Doubt</p>
-
             </div>
         )
     }
